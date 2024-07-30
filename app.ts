1d57a08479b30bc2198212035500680ff3f8cb83
Auspicious14 <uthmanabdulganiyu2019@gmail.com>
2024-07-30 23:58:24 +0100

    [fix] cors

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/auth";
 import categoryRoutes from "./routes/category";
 import professionRoutes from "./routes/profession";
@@ -7,10 +8,25 @@ import typeRoutes from "./routes/type";
 import userRoutes from "./routes/user";
 import { setupSwagger } from "./swagger";
 
-dotenv.config();
-
 const app = express();
+dotenv.config();
+const corsOptions = {
+  origin: "*",
+  credentials: false,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow_Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(authRoutes);
 app.use(categoryRoutes);
--- swagger.ts
+++ swagger.ts
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: "https://career-guidance-be.onrender.com",
+        // url: "https://career-guidance-be.onrender.com",
       },
     ],
   },
